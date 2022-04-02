3e36cc12a9bb479d5ca5392f07096d07ec86a204
Vicky Suprayogi <vsuprayogi@yahoo.com>
2022-04-02 13:14:56 +0700

    WA Chatbot, add link kuitansi

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 var config = {
     TokenExpired : 86400, // expires in 86400 s (24 hours)
-    FolderFile : 'D:\\VICKY\\React JS\\sisqu2\\files\\'
+    FolderFile : 'D:\\VICKY\\React JS\\sisqu2\\files\\',
+    urlApi : 'http://api.pastibaik.com'
 }
 
 export { config };
