072ca48524c19c0b5db2ed10d87d6f02805b4800
Vicky Suprayogi <vsuprayogi@yahoo.com>
2022-06-07 11:51:35 +0700

    library automate config, change url

--- libraries/automate/config.js
+++ libraries/automate/config.js
@@ -1,5 +1,6 @@
 var config = {
-    apiUrl : 'https://qudwah-api.matamantra.id/story/4442/broadcast',
+    // apiUrl : 'https://qudwah-api.matamantra.id/story/4442/broadcast',
+    apiUrl : 'https://notify-qudwah.matamantra.com/notify-sisqu',  // updated 07-Juni-2022
     token : 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2FnZSI6ImFwcF9hY2Nlc3MiLCJpYXQiOjE2NDg3OTM0MDcsInN1YiI6MjMwfQ.fh5KJXQklP4R0uLKkYYpKvyhhl5DOMNrpcsNQg1ZS5pVRLl4azmJzeV14foIws8GBF42J52UprIhF57Ox5uwdw'
 }
 
